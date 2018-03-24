d287fc68dc1a0984d578ebf013f588bae7e3c760
Fabio Romanini <fabioaromanini@gmail.com>
2018-03-24 11:16:31 -0300

    0.9.2

--- docker.sh
+++ docker.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # TODO:
-# move images dir to separated disk
-# add user to docker group
-# install docker-machine
-# install docker-compose
-# auto complete
 
-DOCKER_VERSION="docker-ce_18.02.0~ce-0~ubuntu_amd64"
-DOCKER_MNT="/mnt/docker"
+
+DOCKER_VERSION="docker-ce_18.03.0~ce-0~ubuntu_amd64"
 DOCKER_RELEASE_TYPE="edge"
+DOCKER_MACHINE_VERSION="v0.14.0"
+DOCKER_MNT="/mnt/docker"
 
 DOWNLOADS_DIR="$HOME/Downloads/workspace/"
 FILE_NAME="$DOCKER_VERSION.deb"
 DOCKER_PATH="$DOWNLOADS_DIR$FILE_NAME"
+DOCKER_DOWNLOAD_PATH="https://download.docker.com/linux/ubuntu/dists/xenial/pool/$DOCKER_RELEASE_TYPE/amd64/$FILE_NAME"
 
 if [ ! -d $DOCKER_MNT ]; then
 	echo "Installation requires $DOCKER_MNT"
@@ -22,10 +20,9 @@ else
 	echo "disk mounted"
 fi
 
-
 if [ ! -f $DOCKER_PATH ]; then
     echo "Docker pkg not found!"
-	wget -P $DOWNLOADS_DIR https://download.docker.com/linux/ubuntu/dists/xenial/pool/$DOCKER_RELEASE_TYPE/amd64/$FILE_NAME
+	wget -P $DOWNLOADS_DIR $DOCKER_DOWNLOAD_PATH
 else
     echo "Docker pkg found!"
 fi
@@ -40,8 +37,30 @@ else
 	echo "Docker package is already installed"
 fi
 
-# moves docker volumes to DOCKER_MNT, in order to preserve space in OS disk
+sudo usermod -aG docker $USER
+
+#installs docker-machine
+curl -L https://github.com/docker/machine/releases/download/$DOCKER_MACHINE_VERSION/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+    chmod +x /tmp/docker-machine &&
+    sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
+
+# installs docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# autocomplete
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.20.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+# moves docker's volumes and images to DOCKER_MNT, in order to preserve space in OS disk
 sudo systemctl stop docker
-sudo mv /var/lib/docker/volumes $DOCKER_MNT/
-sudo ln -s $DOCKER_MNT/volumes /var/lib/docker/volumes
+
+mkdir $DOCKER_MNT/docker-config
+
+sudo mv /var/lib/docker/volumes $DOCKER_MNT/docker-config/
+sudo ln -s $DOCKER_MNT/docker-config/volumes /var/lib/docker/volumes
+
+sudo mv /var/lib/docker/image $DOCKER_MNT/docker-config/
+sudo ln -s $DOCKER_MNT/docker-config/images /var/lib/docker/image
+
 sudo systemctl start docker
